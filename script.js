47568d38ad74dad52b2a9983c7339b1d9e55ecbc
aadebh <35617074@student.murdoch.edu.au>
2025-03-02 10:40:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 document.getElementById("project-description").innerText = 
-`The Cloud-Based Web Hosting Project is designed to showcase a static website deployment to a cloud server. This project focuses on the practical aspects of cloud computing, server management, and website deployment. With a little help from cloud services like AWS EC2, Microsoft Azure, Google Cloud, or DigitalOcean, we aim to illustrate the real-life case scenarios of virtual machines (VM) and cloud infrastructure. The different activities approached here revolve around setting up a virtual machine (VM), configuring it with a Linux-based web server like Apache or Nginx, and deploying a static website portfolio or information page consisting of HTML, CSS, and JavaScript. Information about cloud service provider configurations, network settings, security mechanisms, and more will be included to ensure that the website can be accessed through a public IP address or domain name. 
+    "The Cloud-Based Web Hosting Project is designed to showcase a static website deployment to a cloud server. " +
+    "This project focuses on the practical aspects of cloud computing, server management, and website deployment. " +
+    "With a little help from cloud services like AWS EC2, Microsoft Azure, Google Cloud, or DigitalOcean, we aim to illustrate " +
+    "the real-life case scenarios of virtual machines (VM) and cloud infrastructure. The different activities approached here " +
+    "revolve around setting up a virtual machine (VM), configuring it with a Linux-based web server like Apache or Nginx, and " +
+    "deploying a static website portfolio or information page consisting of HTML, CSS, and JavaScript. Information about cloud " +
+    "service provider configurations, network settings, security mechanisms, and more will be included to ensure that the website " +
+    "can be accessed through a public IP address or domain name.\n\n" +
 
-The key functions that make up the project include the following: The setup of the virtual machine includes the selection of the cloud service provider and subsequent launching of the instance, which has the operating system installed (ideally Ubuntu or CentOS); A web server installation and configuration that permits the static website files to be properly hosted; The configuration of networking and security includes assignment of static IP, firewall rules, and SSH authentication for secure access; website deployment of static files, testing of accessibility, and configuration of maintenance scripts for monitoring the live performance. 
+    "The key functions that make up the project include the following: The setup of the virtual machine includes the selection " +
+    "of the cloud service provider and subsequent launching of the instance, which has the operating system installed (ideally Ubuntu or CentOS); " +
+    "A web server installation and configuration that permits the static website files to be properly hosted; The configuration of networking " +
+    "and security includes assignment of static IP, firewall rules, and SSH authentication for secure access; website deployment of static files, " +
+    "testing of accessibility, and configuration of maintenance scripts for monitoring the live performance.\n\n" +
 
-Many advantages over traditional hosting exist in cloud computing, such as scalability, cheaper prices, and much greater uptime. This project thereby gives students experiential learning in managing cloud services, administering Linux servers, hosting websites, and implementing some security measures towards their maintenance. Successful deployment of the static website shall act as a stepping stone for students to cloud-based applications, such as dynamic sites, databases, cloud storage, and API hosting.;
+    "Many advantages over traditional hosting exist in cloud computing, such as scalability, cheaper prices, and much greater uptime. " +
+    "This project thereby gives students experiential learning in managing cloud services, administering Linux servers, hosting websites, " +
+    "and implementing some security measures towards their maintenance. Successful deployment of the static website shall act as a stepping stone " +
+    "for students to cloud-based applications, such as dynamic sites, databases, cloud storage, and API hosting.";
 
 document.getElementById("license-text").innerText = 
-    `MIT License
+`MIT License
 
 Copyright (c) 2025 Aadesh Bhandari
 
